46c6043913e54be82ee5f0c068f7e687194f6ae7
Henryque <henryque_1@hotmail.com>
2021-02-23 12:32:51 -0300

    Database creation(sqlite) + files created (controllers, database, models and routes) + typeorm and uuid files added

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello World - NLW04"});
-});
-
-app.post("/", (req, res) => {
-    return res.json({ message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(4000, () => console.log('Server is running normally!'));
