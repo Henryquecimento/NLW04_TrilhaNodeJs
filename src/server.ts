ece921ee53f7c922061454929fe54dcb3ce54e7b
Henryque <henryque_1@hotmail.com>
2021-02-23 08:08:02 -0300

    server created + project configuration -- 23/02/21

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello World - NLW04"});
+});
+
+app.post("/", (req, res) => {
+    return res.json({ message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(4000, () => console.log('Server is running normally!'));
